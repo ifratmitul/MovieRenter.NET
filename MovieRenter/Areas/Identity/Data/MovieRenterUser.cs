339d0824c8d0b4f1bd2c46d1de2ffc5599a93396
ifrat Mitul <ifrat.mitul@hotmail.com>
2020-07-24 14:59:39 +0600

    userDb, registrting, Login Done

---
+++ MovieRenter/Areas/Identity/Data/MovieRenterUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace MovieRenter.Areas.Identity.Data
+{
+    // Add profile data for application users by adding properties to the MovieRenterUser class
+    public class MovieRenterUser : IdentityUser
+    {
+        [PersonalData]
+        [Column(TypeName = ("nvarchar(100)"))]
+        public  string Fname { get; set; }
+        public string Lname { get; set; }
+    }
+}
---
+++ MovieRenter/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MovieRenter.Areas.Identity.Data;
+using MovieRenter.Data;
+
+[assembly: HostingStartup(typeof(MovieRenter.Areas.Identity.IdentityHostingStartup))]
+namespace MovieRenter.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserDbContextConnection")));
+
+                services.AddDefaultIdentity<MovieRenterUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<UserDbContext>();
+            });
+        }
+    }
+}
